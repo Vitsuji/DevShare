ef72b9d34febf9cf76c410118a120421d22ba8ee
Vitsuji <vitsuji123@gmail.com>
2018-08-14 16:34:24 +0300

    Added Project logic and put Project relationship in home template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Project;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $projects = Project::orderBy('id')->paginate(10);
+        return view('home')->withProjects($projects);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,17 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
+                    @if (auth()->check())
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
                         </div>
+                        <h1>Welcome {{ explode(" ", Auth::user()->name)[0] }}</h1>
+                        @foreach ($projects as $project)
+                            <h2>{{ $project->description }}</h2>
+                            {{--  Call the user method and then selelct what you need from the user table e.g name, id, profile pic --}}
+                            <h4>{{ $project->user->name}}</h4>
+                        @endforeach
+                    @else
+                        <h3>Not logged</h3>
                     @endif
 
                     You are logged in!
