15220df647c64f8b6ab36fdbb2dcbe8f2ddc0e6b
Max Ungless <maxungless@gmail.com>
2018-08-18 13:06:37 +0100

    Updated nav with centered icons

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -1,19 +1,21 @@
 <nav>
-        <h1><a href="{{ url('/') }}">Emineto</a></h1>
-        <section id="nav__items">
-            <a href="{{ url('/projects/') }}"><i class="far fa-compass"></i> <span class="nav__support-text">Explore</span></a>
+    <h1><a href="{{ url('/') }}">Emineto</a></h1>
+    <section id="nav__items">
+        <a href="{{ url('/projects/') }}"><i class="far fa-compass"></i> <span class="nav__support-text">Explore</span></a>
+        @if(auth()->check())
             <a href="{{ url('/projects/') }}"><i class="far fa-file-alt"></i> <span class="nav__support-text">My Projects</span></a>
-            <a href="{{ url('/projects/create/') }}"><i class="fas fa-plus"></i> <span class="nav__support-text">Add Project</span></a>
-            @if(auth()->check())
-                <section id="nav__user">
-                    <a href="{{ url('/home/') }}">{{ Auth::user()->name }}</a>
-                    <a href="{{ url('/logout/') }}">Logout</a>
-                </section>
-            @else
-                <section id="nav__logged-out">
-                    <a href="{{ url('/login/') }}">Login</a>
-                    <a href="{{ url('/register/') }}">Register</a>
-                </section>
-            @endif
+        <a href="{{ url('/projects/create/') }}"><i class="fas fa-plus"></i> <span class="nav__support-text">Add Project</span></a>
+        @endif
+    </section>
+    @if(auth()->check())
+        <section id="nav__user">
+            <a href="{{ url('/home/') }}">{{ Auth::user()->name }}</a>
+            <a href="{{ url('/logout/') }}">Logout</a>
         </section>
+    @else
+        <section id="nav__logged-out">
+            <a href="{{ url('/login/') }}">Login</a>
+            <a href="{{ url('/register/') }}">Register</a>
+        </section>
+    @endif
 </nav>
